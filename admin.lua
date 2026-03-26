24e38360ee7e18be70c73d38ab834baec90d6acf
Traplixy <ysteal.aternos@gmail.com>
2026-03-26 07:15:10 -0700

    Add initial admin script for SiteLaunchers

---
+++ admin.lua
@@ -0,0 +1,9 @@
+-- SiteLaunchers Universal Admin Loadstring
+print("SiteLaunchers: Script Successfully Loaded!")
+
+-- Add your actual script logic below
+game.StarterGui:SetCore("SendNotification", {
+    Title = "SiteLaunchers";
+    Text = "Script Activated!";
+    Duration = 5;
+})
